dd66de2fc965d6d9d8ddb0ad3319754aa210a84a
sam <sam@lndata.com>
2020-02-13 00:39:31 -0500

    Fixed not importing db when running the app.

--- db.py
+++ db.py
@@ -3,3 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 # thing that going to link to the flask app, and look up the object and connect to database
 
+@app.before_first_request
+def create_tables():
+	db.create_all()
