496fb482ef0db126a4e3522ae6b223bca4a226bc
sam <sam@lndata.com>
2020-02-13 00:59:26 -0500

    Modified the @app from app.py.

--- app.py
+++ app.py
@@ -8,16 +8,13 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 
-
 app = Flask(__name__)
+app.config['DEBUG'] = True
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "jose"
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-	db.create_all()
 
 jwt = JWT(app, authenticate, identity) #create new endpoint: /auth
 
@@ -30,4 +27,8 @@ api.add_resource(UserRegister, "/register")
 if __name__ == '__main__':
 	from db import db
 	db.init_app(app)
+	if app.config['DEBUG']:
+		@app.before_first_request
+		def create_tables():
+			db.create_all()
 	app.run(port=5000, debug=True)
